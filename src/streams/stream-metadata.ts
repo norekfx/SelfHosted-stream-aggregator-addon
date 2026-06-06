bc8cc788662ec6ec961c9971fc31d53572a5d4e7
norekfx <noe2201@o2.pl>
2026-06-06 09:09:34 +0200

    feat: add normalized stream metadata types

---
+++ src/streams/stream-metadata.ts
@@ -0,0 +1,42 @@
+export type NormalizedQuality =
+  | "4k"
+  | "2160p"
+  | "1440p"
+  | "1080p"
+  | "720p"
+  | "576p"
+  | "480p"
+  | "360p"
+  | "240p"
+  | "144p"
+  | "unknown";
+
+export type ReleaseSource =
+  | "BluRay"
+  | "WEB-DL"
+  | "WEBRip"
+  | "HDRip"
+  | "DVDRip"
+  | "HDTV"
+  | "CAM"
+  | "TS"
+  | "TC"
+  | "unknown";
+
+export type VideoCodec = "AV1" | "HEVC" | "H264" | "XVID" | "MPEG2" | "unknown";
+
+export type AudioKind = "original" | "lektor" | "dubbing" | "subbed" | "multi" | "unknown";
+
+export type NormalizedStreamMetadata = {
+  quality: NormalizedQuality;
+  source: ReleaseSource;
+  videoCodec: VideoCodec;
+  audioLanguage?: string;
+  subtitleLanguage?: string;
+  audioKind: AudioKind;
+  isMultiLanguage: boolean;
+  releaseGroup?: string;
+  size?: string;
+  rawText: string;
+  matchedTokens: string[];
+};
